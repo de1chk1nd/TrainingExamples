e8aa2370a9bb041f196e6feb98a0f8f76e7d063c
de1chk1nd <martin.erbloweit@gmail.com>
2021-06-24 10:11:22 +0200

    main

--- Volterra/tools/load_gen.py
+++ Volterra/tools/load_gen.py
@@ -19,7 +19,7 @@ user_agents = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36 Edg/91.0.864.54"
 ]
 
-count = 2
+count = 1000
 
 os.system("cls")
 
